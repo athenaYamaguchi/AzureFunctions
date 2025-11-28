7be458b306cb3043bcb366117e10ad4a6cf3b217
unknown <t-yamaguchi@MAINOFFICE.local>
2025-11-28 12:54:11 +0900

    test

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-// src/index.ts
-import { app } from "@azure/functions";
-import httpTrigger1 from "./functions/httpTrigger1";
-
-app.http("httpTrigger1", {
-  methods: ["GET", "POST"],
-  authLevel: "function",
-  handler: httpTrigger1
-});
