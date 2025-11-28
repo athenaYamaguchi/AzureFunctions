7d02c3e7ee9bba7fd8fc80d5b1d8a74ebc1e6d92
unknown <t-yamaguchi@MAINOFFICE.local>
2025-11-28 11:39:34 +0900

    aaaaa

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
-import { app } from '@azure/functions';
+// src/index.ts
+import { app } from "@azure/functions";
+import httpTrigger1 from "./functions/httpTrigger1";
 
-app.setup({
-    enableHttpStream: true,
+app.http("HttpTrigger1", {
+  methods: ["GET", "POST"],
+  authLevel: "function",
+  handler: httpTrigger1
 });
