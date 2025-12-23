f2b7c2b6494e76179a6427b95c21bee0228b1a7c
athenaYamaguchi <athenasystemsyt@gmail.com>
2025-12-23 11:22:19 +0900

    test

---
+++ src/functions/HttpTrigger2.ts
@@ -0,0 +1,22 @@
+
+import { HttpRequest, InvocationContext } from "@azure/functions";
+import * as sql from "mssql";
+
+export default async function (req: HttpRequest, ctx: InvocationContext) {
+  const config: sql.config = {
+    user: process.env.SQL_USER,
+    password: process.env.SQL_PASSWORD,
+    database: process.env.SQL_DATABASE,
+    server: process.env.SQL_SERVER,
+    options: { encrypt: true, enableArithAbort: true }
+  };
+
+  try {
+    const pool = await sql.connect(config);
+    const result = await pool.request().query("SELECT * FROM ATHENA_WEB.M_Users");
+    return { status: 200, jsonBody: result.recordset };
+  } catch (e) {
+    ctx.error(e);
+    return { status: 500, body: "Server errorr" };
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,17 @@
 // src/index.ts
 import { app } from "@azure/functions";
 import HttpTrigger from "./functions/HttpTrigger";
+import HttpTrigger2 from "./functions/HttpTrigger2";
 
 app.http("HttpTrigger", {
   methods: ["GET", "POST"],
   authLevel: "anonymous",
   handler: HttpTrigger
 });
+
+app.http("HttpTrigger2", {
+  methods: ["GET", "POST"],
+  authLevel: "anonymous",
+  handler: HttpTrigger2
+});
+
